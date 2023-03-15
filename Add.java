f7ec366f4b4bda7654413e2df40852ec8326072c
Ujwala4558 <112870659+Ujwala4558@users.noreply.github.com>
2023-03-15 13:33:26 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+class Add{
+public static void main(String []args){
+System.out.println("The two number is:");
+
+int a=8;
+int b=5;
+System.out.println("The value a is 8");
+System.out.println("The value a is 5");
+
+int sum=a+b;
+int mul=a*b;
+System.out.println("The sum is="+sum);
+System.out.println("The mul is="+mul);
+}
+}
---
+++ AreaofRectangle.java
@@ -0,0 +1,8 @@
+public class AreaofRectangle{
+public static void main(String args[])
+{
+int a=7;
+int b=3,c;
+c=a*b;
+System.out.println(" AreaofRectangle is:" +c);
+}}
